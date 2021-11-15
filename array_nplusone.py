26c3a36fe5658f7f7f2fd650b5e260a392bab481
azukhrufy <anandazukhrufawalwi@gmail.com>
2021-11-15 20:31:08 +0700

    add array n + 1 program

---
+++ array_nplusone.py
@@ -0,0 +1,18 @@
+s = [1,4,6,8,9,3,5]
+d = 4
+m = 2
+
+index = 0
+sum = 0
+match = 0
+
+for i in range(len(s)):
+    sum = 0
+    for x in range(m):
+        #first get i index whitout add x
+            print(x + i)
+            
+    if sum == d:
+        match += 1
+
+print(match)
