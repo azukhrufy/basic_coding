1f079c89b2760e3452a968aad5838d8a066992fe
azukhrufy <anandazukhrufawalwi@gmail.com>
2021-11-25 09:23:34 +0700

    add the simplest version of count_substring

--- count_substring.py
+++ count_substring.py
@@ -1,20 +1,9 @@
 def count_substring(string, sub_string):
-    s = list(sub_string)
     count = 0
-    res = 0
-    for i in range(0,len(string)):
-        if string[i] == s[0]:
-            index = i
-            count = 0
-            for x in range(0,len(sub_string)):
-                if string[index] == sub_string[x]:
-                    count += 1
-                if index < len(string) -1:
-                    index += 1
-            if count == len(sub_string):
-                res += 1
-    
-    return res
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
+            count += 1
+    return count
 
 if __name__ == '__main__':
     string = input().strip()
